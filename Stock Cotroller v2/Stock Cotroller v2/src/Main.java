fac3feb57eb7303758777511eb505faf57ccb2a4
Filipe Silva <118104038+FilipeSilva21@users.noreply.github.com>
2024-12-10 00:16:42 -0300

    Update Main.java

--- Stock Cotroller v2/Stock Cotroller v2/src/Main.java
+++ Stock Cotroller v2/Stock Cotroller v2/src/Main.java
@@ -4,27 +4,63 @@ import services.AddService;
 import services.RemoveService;
 import services.ViewItensService;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         AddService addService = new AddService();
         RemoveService removeService = new RemoveService();
         ViewItensService viewItensService = new ViewItensService();
         StockController stockController = new StockController();
+        Scanner scan = new Scanner(System.in);
 
-        addService.addProduct(1, "Iphone", 150, 6500);
-        addService.addProduct(2, "Macbook", 100, 7000);
-        addService.addProduct(3, "Ipad", 125, 5500);
+        System.out.println("Escolha uma opcao: ");
+        System.out.println("1. Adicionar um produto");
+        System.out.println("2. Remover um produto");
+        System.out.println("3. Ver a quantidade de um produto");
+        System.out.println("4. Ver todos os itens em estoque");
+        System.out.println("5. Sair");
+        int choice = scan.nextInt();
 
-        System.out.println("");
+        switch (choice){
+            case (1):
+                System.out.println("Informe o Id do produto");
+                int Id = scan.nextInt();
+                scan.nextLine();
+                System.out.println("Informe o nome do produto");
+                String name = scan.nextLine();
+                System.out.println("Informe a quantidade do produto");
+                int quant = scan.nextInt();
+                System.out.println("Informe o preco do produto");
+                int price = scan.nextInt();
+                addService.addProduct(Id, name, quant ,price);
+            break;
 
-        removeService.removeProduct("Iphone", stockController.findById(1));
+            case (2):
+                System.out.println("Informe o nome do produto");
+                String name1 = scan.nextLine();
+                System.out.println("Informe o Id do produto");
+                int Id1 = scan.nextInt();
+                removeService.removeProduct(name1, Id1);
+            break;
 
-        System.out.println("");
+            case (3):
+                System.out.println("Informe o Id do produto");
+                int Id2 = scan.nextInt();
+                viewItensService.viewQuantity(Id2);
+            break;
 
-        viewItensService.viewQuantity(2);
+            case (4):
+                viewItensService.listItens();
+            break;
 
-        System.out.println("");
+            case (5):
+                System.out.println("Saindo...");
+                return;
 
-        viewItensService.listItens();
+            default:
+                System.out.println("Opcao invalida. Tente novamente.");
+        }
+
     }
 }
